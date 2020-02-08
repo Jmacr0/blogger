4a165f77d80f04565507312a79d81df460cd62cf
Jmacro <jonmacrohon@gmail.com>
2020-02-08 20:35:50 +1100

    added delete ajax request to like event hander

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,27 +1,32 @@
 // Shorthand for $( document ).ready()
 $(function () {
-    console.log("ready!");
+	console.log("ready!");
 
-    $("#login-button").on("click", function () {
-        console.log("clicked");
-        //need to add some logic
-    });
+	$("#login-button").on("click", function () {
+		console.log("clicked");
+		//need to add some logic
+	});
 
-    $("#sign-up-button").on("click", function () {
-        //need to add some logic
-        console.log("clicked");
-    });
+	$("#sign-up-button").on("click", function () {
+		//need to add some logic
+		console.log("clicked");
+	});
 
-    $("i").on("click", function () {
+	$("i").on("click", function () {
 		event.preventDefault();
-		const PostId = $(this).data("postId");
+		const PostId = $(this).siblings("h5").data("postId");
 		console.log(PostId, "JQuery")
-        if($(this).hasClass("far")){
-			$(this).removeClass( "far" ).addClass( "fas" );
-			$.post("/post/like", {PostId: PostId} )
-            
-        } else {
-            $(this).removeClass( "fas" ).addClass( "far" );  
-        }
-    })
+		if ($(this).hasClass("far")) {
+			$(this).removeClass("far").addClass("fas");
+			$.post("/post/like", { PostId: PostId })
+
+		} else {
+			$(this).removeClass("fas").addClass("far");
+			$.ajax({
+				url: "/post/like",
+				type: "DELETE",
+				data: {PostId: PostId}
+			});
+		}
+	})
 });
