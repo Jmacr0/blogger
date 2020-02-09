5be285c5374f3aa70ac56833c2478d4f7ea475f7
Jmacro <jonmacrohon@gmail.com>
2020-02-10 10:45:04 +1100

    removed commented out hbs helper

--- server.js
+++ server.js
@@ -43,45 +43,6 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// var hbs = exphbs.create({
-//     // Specify helpers which are only registered on this instance.
-//     helpers: {
-//         foo: function () { return 'FOO!'; },
-// 		bar: function () { return 'BAR!'; },
-// 		compare: function (lvalue, operator, rvalue, options) {
-// 			var operators, result;
-// 			if (arguments.length < 3) {
-// 				throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
-// 			}
-// 			if (options === undefined) {
-// 				options = rvalue;
-// 				rvalue = operator;
-// 				operator = "===";
-// 			}
-// 			operators = {
-// 				'==': function (l, r) { return l == r; },
-// 				'===': function (l, r) { return l === r; },
-// 				'!=': function (l, r) { return l != r; },
-// 				'!==': function (l, r) { return l !== r; },
-// 				'<': function (l, r) { return l < r; },
-// 				'>': function (l, r) { return l > r; },
-// 				'<=': function (l, r) { return l <= r; },
-// 				'>=': function (l, r) { return l >= r; },
-// 				'typeof': function (l, r) { return typeof l == r; }
-// 			};
-// 			if (!operators[operator]) {
-// 				throw new Error("Handlerbars Helper 'compare' doesn't know the operator " + operator);
-// 			}
-// 			result = operators[operator](lvalue, rvalue);
-// 			if (result) {
-// 				return options.fn(this);
-// 			} else {
-// 				return options.inverse(this);
-// 			}
-// 		}
-//     }
-// });
-
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
